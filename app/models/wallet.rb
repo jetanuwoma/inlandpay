7e87783886edb1b9b6573cb279bdbbff1a2576b1
Jude Etanuwoma <jude.etanuwoma@andela.com>
2021-01-23 23:32:23 +0100

    Add bitcoin Service and generate btc address for user

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -3,6 +3,7 @@
 class Wallet < ApplicationRecord
   enum is_default: %i[Yes No]
 
+  validates_uniqueness_of :number, conditions: -> { where.not(number: nil) }
   validates_presence_of :balance, :is_default
   validates_associated :currency, :user
 
@@ -10,11 +11,19 @@ class Wallet < ApplicationRecord
   belongs_to :currency
 
   before_create :assign_number
+  after_create :generate_btc_address
 
   private
 
   def assign_number
     code = currency.code
     self.number = rand(10**10) if code == 'NGR'
+  end
+
+  def generate_btc_address
+    code = currency.code
+    return true unless code == 'BTC'
+
+    BitcoinService.new(user_id: user.id, currency_id: currency.id).generate_address
   end
 end
---
+++ app/services/bitcoin_service.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# Bitcoin operation service
+class BitcoinService
+  def initialize(user_id:, currency_id: nil)
+    @user_id = user_id
+    @currency_id = currency_id
+    @blockcypher = BlockCypher::Api.new(api_token: ENV['BLOCKCYPHER_TOKEN'], network: BlockCypher::TEST_NET_3)
+  end
+
+  def generate_address
+    address = @blockcypher.address_generate
+    response = address.with_indifferent_access
+    wallet = Wallet.find_by!(currency_id: @currency_id, user_id: @user_id)
+    wallet.number = "#{response[:address]}:#{response[:private]}"
+    wallet.save
+  rescue StandardError => e
+    puts e.message
+    false
+  end
+end
