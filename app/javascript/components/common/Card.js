545e2ed0199d5894e2de38fd2f8cfee8901e4aba
Jude Etanuwoma <jude.etanuwoma@andela.com>
2021-01-25 09:56:33 +0100

    Add scheduler to update btc Display wallet balance on user dashboard

---
+++ app/javascript/components/common/Card.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Card extends  React.Component {
+    render() {
+        const { name, balance, code } = this.props;
+        return (
+          <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 mb-20">
+              <div className="card">
+                  <div className="card-header">
+                      {code}
+                  </div>
+                  <div className="card-body">
+                      <p>{name}</p>
+                      <p>Balance: <strong>{balance}</strong></p>
+                  </div>
+              </div>
+          </div>
+        )
+    }
+}
+
+Card.propTypes = {
+    name: PropTypes.string.isRequired,
+    balance: PropTypes.any.isRequired,
+    code: PropTypes.string.isRequired,
+}
+
---
+++ app/workers/bitcoin_balance_worker.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal:true
+
+# Bitcoin balance worker - updates user's btc balance
+class BitcoinBalanceWorker
+  include Sidekiq::Worker
+
+  def perform
+    btc = Currency.find_by(code: 'BTC')
+    addresses = Wallet.where(currencies: btc).where.not(number: nil).pluck(:number)
+    addresses.each do |addrs|
+      Async do
+        balance = BitcoinService.new.check_balance(addrs)
+        if balance
+          hashed = balance.with_indifferent_access
+          wallet = Wallet.find_by!(number: addrs)
+          wallet.balance = hashed[:final_balance]
+          wallet.save!
+        end
+      end
+    end
+  end
+end
